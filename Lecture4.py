6714014552391970856121d6efc3a2717eeba8da
khutsishviligv-design <khutsishvili.gv@gmail.com>
2026-05-06 20:21:31 +0400

    Update Lecture4.py

--- Lecture4.py
+++ Lecture4.py
@@ -5,3 +5,17 @@
 უფროსი (65+)
 ბოლოს დაბეჭდე შედეგი, მაგალითად:
 "თქვენ ხართ: თინეიჯერი"""
+
+age=int(input("Please enter your age:"))
+if age  >=0 and age <=12:
+    status="Child"
+elif age >=13 and age <=19:
+    status="Teenager"
+elif age >= 20 and age <= 64:
+    status="Adult"
+elif age >= 65:
+    status = "Senior"
+else:
+    status = "not correct age"
+
+print("You are:", status)
